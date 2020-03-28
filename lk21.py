8dba844efacbeafb44b940c79dde4a6117560efd
kuronekony4n <38156932+kuronekony4n@users.noreply.github.com>
2020-03-28 18:47:34 +0700

    Update lk21.py

--- lk21.py
+++ lk21.py
@@ -66,7 +66,7 @@ def bypass(url,judul):
 	'accept':'*/*',
 	'origin':f'http://{blin2}',
 	'x-requested-with':'XMLHttpRequest',
-	'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+	'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
 	'content-type':'application/x-www-form-urlencoded;charser=UTF-8',
 	'referer':link2,
 	'accept-encoding':'gzip, deflate, br',
